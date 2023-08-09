243e38c3a6bab245eb361492e345e96e8643d881
Renato Nobrega <renatospinola762@gmail.com>
2023-08-09 12:39:30 -0300

    criado saque, deposito e rendimento

---
+++ src/Conta.java
@@ -0,0 +1,30 @@
+
+public class Conta {
+	String titular;
+	int numero;
+	String agencia;
+	double saldo;
+	String dataAbertura;
+
+	void sacar(double quantidade) {
+		if (quantidade > this.saldo) {
+			System.out.println("Saldo insuficiente.");
+		} else {
+			this.saldo -= quantidade;
+			System.out.println("Saque de " + quantidade + " Efeutado com sucesso" + "\nSaldo Atual: " + this.saldo);
+		}
+	}
+
+	void depositar(double quantidade) {
+		this.saldo += quantidade;
+		System.out.println("Valor depositado " + quantidade + "\nSaldo atual: " + this.saldo);
+	}
+	
+	void rendimento() {
+		
+		this.saldo += this.saldo * 0.1;
+		System.out.println("Sua conta rendeu 0.1% e agora você tem: " + this.saldo);
+		
+	}
+
+}
---
+++ src/main.java
@@ -0,0 +1,14 @@
+
+public class main {
+
+	public static void main(String[] args) {
+		Conta c1 = new Conta();
+		
+		c1.depositar(1200);
+		c1.sacar(700);
+		
+		c1.rendimento();
+
+	}
+
+}
